66d9b417a2de5d80dd8a8f8bcc6e4ef34b34505c
DSLEGEND2403 <71708093+DSLEGEND2403@users.noreply.github.com>
2020-12-11 17:10:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ leftWristY= 0;
 rightWristX= 0;
 rightWristY= 0;
 scoreLeftWrist=0;
-scorerightWrist=0;
+scoreRightWrist=0;
 SWIP="";
 
 function preload()
@@ -29,16 +29,34 @@ function draw()
 image(video, 0, 0, 600, 500);
 fill("#FF0000");
 stroke("#FF0000"); 
+
+
 Songstatus = Song1.isPlaying();
 
 if (scoreLeftWrist > 0.2)
 {
+console.log("Inside The Score Left Wrist Condition "+ Songstatus);    
 circle(leftWristX,leftWristY,20);
 Song2.stop();  
 if (Songstatus == false)
 {
-Song1.play(); 
+Song1.play();
+console.log("Song1 is playing"); 
 document.getElementById("songname").innerHTML = "Born For this";
+}    
+}  
+Songstatus2 = Song2.isPlaying();
+
+if (scoreRightWrist > 0.2)
+{
+console.log("Inside The Score Right Wrist Condition "+ Songstatus2);    
+circle(rightWristX,rightWristY,20);
+Song1.stop();  
+if (Songstatus2 == false)
+{
+Song2.play();
+console.log("Song2 is playing");  
+document.getElementById("songname").innerHTML = "Ninjago Season 5 Intro";
 }    
 }  
 }
